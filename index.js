0cec027618cc31141331b1d9c882578081c1772d
Jeff Lin <thejefflin@gmail.com>
2022-04-20 00:18:07 -0400

    commit tree 16cb7417bc647686b8af670d0671838a812a8b8f

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+const text = document.getElementById('text');
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  text.textContent = `This is really cool!`
+  })
